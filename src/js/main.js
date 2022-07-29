8c978a4422fed06375a8dd2e3e4862ce2e42c39e
proflinervk <proflinervk@gmail.com>
2022-07-29 16:52:49 +0300

    first

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+// global
+import './common/global'
+// sections
+import './sections/header'
+import './sections/portfolio'
---
+++ src/js/sections/portfolio.js
@@ -0,0 +1,17 @@
+import mediumZoom from 'medium-zoom'
+
+document.addEventListener( 'DOMContentLoaded', () => {
+	'use strict'
+
+	mediumZoom(images)
+})
+
+const images = [document.querySelectorAll('.zoom')]
+
+mediumZoom('.zoom',  {
+	margin: 20,
+	background: '#000000',
+	scrollOffset: 0  
+})  
+
+
