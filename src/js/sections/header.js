10ee9521ede4f5228dd373afcc15240c16001763
proflinervk <proflinervk@gmail.com>
2022-07-30 04:15:25 +0300

    90% complete

--- src/js/sections/header.js
+++ src/js/sections/header.js
@@ -1,5 +1,30 @@
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock'
+
 document.addEventListener( 'DOMContentLoaded', () => {
 	'use strict'
 
+})
+
+const navButton = document.querySelector( '.burger-button' )
+const navItems  = document.querySelector( '.nav-items' )
+const lang = document.querySelector( '.header-lang')
+
+navButton.addEventListener('click', () => {
+
+	if ( ! navButton.classList.contains('active')) { 
+		navButton.classList.add('active') 
+	}	else {
+			navButton.classList.remove('active')
+	}
+	if ( ! navItems.classList.contains('opened')) {
+		navItems.classList.add('opened')
+	}	else {
+			navItems.classList.remove('opened')	
+	}
+	if ( ! lang.classList.contains('opened')) {
+		lang.classList.add('opened')
+	}	else {
+			lang.classList.remove('opened')	
+	}
 })
 
