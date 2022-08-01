b5ec39f08a83f52c8e9d0a265b7aac7bd7863f9a
proflinervk <proflinervk@gmail.com>
2022-08-01 21:16:37 +0300

    test

--- src/js/sections/header-scroll.js
+++ src/js/sections/header-scroll.js
@@ -3,22 +3,16 @@ document.addEventListener( 'DOMContentLoaded', () => {
 
 })
 
-const header = document.querySelector( '.header' )
-const headerNavigation = document.querySelector( '.header-nav' )
-const navButton = document.querySelector( '.burger-button')
-
 window.addEventListener('scroll', () => {
-    let scrollTop = window.scrollY
+    const scrollTop = window.scrollY
+    const header = document.querySelector( '.header' )
 
     if (scrollTop > 0) {
-        if (!header.classList.contains( 'scrolled' )) {
+        if ( ! header.classList.contains( 'scrolled' ) ) 
             header.classList.add( 'scrolled' )
-            navButton.classList.add( 'scrolled' )
-        }
+
     }   else {
-        if (header.classList.contains( 'scrolled' )) {
+        if ( header.classList.contains( 'scrolled' ) ) 
             header.classList.remove( 'scrolled' )
-            navButton.classList.remove( 'scrolled' )
-        }
     }
 })
